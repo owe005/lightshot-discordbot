9b1f7d7e85d418a6b4950523fa93b3f16c4971f2
owe005 <92420030+owe005@users.noreply.github.com>
2021-12-22 21:58:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('!lightshot'):
-        msg = str(message.channel)
         randomurlString = string.ascii_lowercase + string.digits
         randomizing = ''.join(random.choice(randomurlString) for i in range(6))
         url = "https://prnt.sc/" + str(randomizing)
